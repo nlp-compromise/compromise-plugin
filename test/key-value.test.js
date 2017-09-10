64414d944c6d173f648149bdf85126ba99b8f523
spencermountain <spencermountain@gmail.com>
2017-09-10 16:08:43 -0400

    test pass

--- test/key-value.test.js
+++ test/key-value.test.js
@@ -1,49 +1,54 @@
 var test = require('tape');
 // var nlp = require('../lib/nlp');
-const packPrefix = require('../../../src/world/pack/packPrefix');
-const unpackPrefix = require('../../../src/world/pack/unpack');
+const pack = require('../pack');
+const unpack = require('../unpack');
 
-let plurals = {
-  addendum: 'addenda',
-  barracks: 'barracks',
-  beau: 'beaux',
-  leaf: 'leaves',
-  libretto: 'libretti',
-  loaf: 'loaves',
-  man: 'men',
-  matrix: 'matrices',
-  memorandum: 'memoranda',
-  modulus: 'moduli',
-  mosquito: 'mosquitoes',
-  move: 'moves',
-  opus: 'opera',
-  ovum: 'ova',
-  ox: 'oxen',
-  tableau: 'tableaux',
-  thief: 'thieves',
-  tooth: 'teeth',
-  tornado: 'tornados',
-  tuxedo: 'tuxedos',
-  zero: 'zeros'
+let plugin = {
+  name: 'tryplurals',
+  plurals: {
+    addendum: 'addenda',
+    barracks: 'barracks',
+    beau: 'beaux',
+    leaf: 'leaves',
+    libretto: 'libretti',
+    loaf: 'loaves',
+    man: 'men',
+    matrix: 'matrices',
+    memorandum: 'memoranda',
+    modulus: 'moduli',
+    mosquito: 'mosquitoes',
+    move: 'moves',
+    opus: 'opera',
+    ovum: 'ova',
+    ox: 'oxen',
+    tableau: 'tableaux',
+    thief: 'thieves',
+    tooth: 'teeth',
+    tornado: 'tornados',
+    tuxedo: 'tuxedos',
+    zero: 'zeros'
+  }
 };
 
 test('pack-plurals', function(t) {
-  let str = packPrefix(plurals);
-  let unpacked = unpackPrefix(str);
-  t.deepEqual(plurals, unpacked, 'in==out');
+  let str = pack(plugin);
+  let unpacked = unpack(str);
+  t.deepEqual(plugin, unpacked, 'in==out');
   t.end();
 });
 
 test('packPlural-edge-cases', function(t) {
   let input = {
-    aaaaaaa: 'aaaaaaa', //equal
-    aaaaaaaa: 'bb', //shorter
-    aa: 'bbbbbbb', //longer
-    a: 'a and some b' //spaces
-    // 'abba, disco|dance': 'fun, cool|haus' //reserved
+    name: 'tryplurals2',
+    plurals: {
+      aaaaaaa: 'aaaaaaa', //equal
+      aaaaaaaa: 'bb', //shorter
+      aa: 'bbbbbbb', //longer
+      a: 'a and some b' //spaces
+    }
   };
-  let str = packPrefix(input);
-  let output = unpackPrefix(str);
+  let str = pack(input);
+  let output = unpack(str);
   t.deepEqual(input, output, 'in==out');
 
   t.end();
